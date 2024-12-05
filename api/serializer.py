b0b8dd3e2617ff9aabd39d226e07b3eeaf31b813
Julieth221 <darlycuberos17@gmail.com>
2024-12-05 12:26:53 -0500

    Serializador para el modelo student

--- api/serializer.py
+++ api/serializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import programmer
+from .models import programmer, student
 
 class ProgrammerSerializer(serializers.ModelSerializer):
     class Meta:
         model = programmer
         # fields = ('fulll_name',  'nickname', 'age')
+        fields = '__all__'
+        
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = student
         fields = '__all__'
